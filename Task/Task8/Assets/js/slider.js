f5d86d75f9f97a362e41da3c164f5d1d13dd92a0
SurajMhz <suraj.maharjan2024@mic.edu.np>
2025-09-22 08:07:25 +0545

    React Components

--- Task/Task8/Assets/js/slider.js
+++ Task/Task8/Assets/js/slider.js
@@ -3,13 +3,16 @@ let index = 0;
 const slides = document.querySelector(".slides");
 const total = document.querySelectorAll(".slides img").length;
 
-document.querySelector(".nextButton").onclick = () => {
+document.querySelector(".nextButton").onclick = () => {next()};
+document.querySelector(".prevButton").onclick = () => {previous()};
+function next(){
     index = (index + 1) % total;
     slides.style.transform = `translateX(-${index * 404}px)`;
-};
-
-document.querySelector(".prevButton").onclick = () => {
+}
+function previous(){
     index = (index - 1 + total) % total;
     slides.style.transform = `translateX(-${index * 404}px)`;
-};
+}
+
+    // setInterval(next(),1000,hello:Infinity[]);
 
