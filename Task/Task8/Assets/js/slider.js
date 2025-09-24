e2238ccf5abfebd7cfb0aa735dc864c95bf637b2
SurajMhz <suraj.maharjan2024@mic.edu.np>
2025-09-24 11:10:39 +0545

    button

--- Task/Task8/Assets/js/slider.js
+++ Task/Task8/Assets/js/slider.js
@@ -1,18 +1,30 @@
+let index = 0
+const slides = document.querySelector(".slides")
+const total = document.querySelectorAll(".slides img").length
+let startX = 0
+let currentX = 0
+let isDragging = false
 
-let index = 0;
-const slides = document.querySelector(".slides");
-const total = document.querySelectorAll(".slides img").length;
 
-document.querySelector(".nextButton").onclick = () => {next()};
-document.querySelector(".prevButton").onclick = () => {previous()};
-function next(){
-    index = (index + 1) % total;
-    slides.style.transform = `translateX(-${index * 404}px)`;
+document.querySelector(".nextButton").onclick = () => next()
+document.querySelector(".prevButton").onclick = () => previous()
+
+function next() {
+    index = (index + 1) % total
+    slides.style.transition = "transform 0.3s ease"
+    slides.style.transform = `translateX(-${index * 404}px)`
 }
-function previous(){
-    index = (index - 1 + total) % total;
-    slides.style.transform = `translateX(-${index * 404}px)`;
+function previous() {
+    index = (index - 1 + total) % total
+    slides.style.transition = "transform 0.3s ease"
+    slides.style.transform = `translateX(-${index * 404}px)`
 }
+// setInterval(next(),1000,hello:Infinity[]);
+//  setInterval(next,1000);
+//auto move and stop on mouseenter  
+let autoSlide = setInterval(next, 2000)
 
-    // setInterval(next(),1000,hello:Infinity[]);
+slides.addEventListener("mouseenter", () => clearInterval(autoSlide))
+slides.addEventListener("mouseleave", () => autoSlide = setInterval(next, 2000))
+
 
